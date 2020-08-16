4cdeac7e16c956b369d8a9807cf813c9dff9f1ef
Thet Min Ko <minko@minko-mac.minkonet>
2020-08-16 23:12:35 +0800

    POC tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <button className="border rounded-full px-3 py-2 text-gray-900 bg-white">Hello</button>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  // purge: ['./src/**/*.html', './src/**/*.jsx'], // this setting will automatically purge if NODE_ENV is production else it will not do anything
+  purge: { // use this if you want to run even in non-production env
+    enabled: true,
+    content: ['./src/**/*.html', './src/**/*.jsx']
+  },
+  theme: {
+    extend: {
+      colors: {
+        limegreen: '#a8ff60'
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus'] // allow hover, focus for background color
+  },
+  plugins: []
+}
