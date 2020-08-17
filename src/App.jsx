01c9abef3e32b924fec5f6f68dea2de51d653887
Thet Min Ko <minko@minko-mac.minkonet>
2020-08-17 14:16:04 +0800

    Use Tailwindcss with Ant.d

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import './App.css';
+import { Select, Input } from 'antd';
+import { SettingOutlined } from '@ant-design/icons';
+
+const { Option} = Select;
 
 function App() {
+  const selectBefore = (
+    <Select defaultValue="http://" className="select-before">
+      <Option value="http://">http://</Option>
+      <Option value="https://">https://</Option>
+    </Select>
+  );
+  const selectAfter = (
+    <Select defaultValue=".com" className="select-after">
+      <Option value=".com">.com</Option>
+      <Option value=".jp">.jp</Option>
+      <Option value=".cn">.cn</Option>
+      <Option value=".org">.org</Option>
+    </Select>
+  );
+  
   return (
-    <div className="App">
-      <button className="border rounded-full px-3 py-2 text-gray-900 bg-white">Hello</button>
+    <div className="App bg-black px-10">
+      <Input className="mt-4 bg-black" addonBefore="http://" addonAfter=".com" defaultValue="mysite" />
+      <Input className="mt-4" addonBefore={selectBefore} addonAfter={selectAfter} defaultValue="mysite" />
+      <Input addonAfter={<SettingOutlined />} defaultValue="mysite" />
+      <Input addonBefore="http://" suffix=".com" defaultValue="mysite" />
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  corePlugins: {
+    preflight: false // false to disable tailwindcss base styles.  disable to use with antd
+  },
   // purge: ['./src/**/*.html', './src/**/*.jsx'], // this setting will automatically purge if NODE_ENV is production else it will not do anything
   purge: { // use this if you want to run even in non-production env
     enabled: true,
